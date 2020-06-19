67e547fab89b306bc14df0b01904adc4024c1cc8
Dexter <vivekojha961@gmail.com>
2020-06-19 22:17:10 +0530

    fixed issue

--- hotel_management.c
+++ hotel_management.c
@@ -14,12 +14,12 @@ struct node{
    struct node *link;
 };
 struct node *start=NULL;
-struct node *checkin(struct node *start);   
+// struct node *checkin(struct node *start);   
 struct node *checkout(struct node *start );
-struct node *edit_info(struct node *start,int guestid);
-struct node *checkguestlist(struct node *start);
-struct node *searchlist(struct node *start,int guestid);
-struct node *check_if_room_available(struct node *start,int room_num);
+// struct node *edit_info(struct node *start,int guestid);
+// struct node *checkguestlist(struct node *start);
+// struct node *searchlist(struct node *start,int guestid);
+// struct node *check_if_room_available(struct node *start,int room_num);
 int main(){
     int choice,guestid,room_num;
     
@@ -34,28 +34,28 @@ int main(){
 		switch(choice)
 		{
 			case 1:
-				start=checkin(start);
+				// start=checkin(start);
 				break;
 			case 2:
 				start=checkout(start);
 				break;
 			case 3:
-				printf("\nENTER GUEST ID\n");
-				scanf("%d",&guestid);
-				start=edit_info(start,guestid);
+				// printf("\nENTER GUEST ID\n");
+				// scanf("%d",&guestid);
+				// start=edit_info(start,guestid);
 				break;
 			case 4:
-				start=checkguestlist(start);
+				// start=checkguestlist(start);
 				break;
 			case 5:
-				printf("\nENTER GUEST ID\n");
-				scanf("%d",&guestid);
-				start=searchlist(start,guestid);
+				// printf("\nENTER GUEST ID\n");
+				// scanf("%d",&guestid);
+				// start=searchlist(start,guestid);
 				break;
 			case 6:
-				printf("\nENTER ROOM NUMBER\n");
-				scanf("%d",&room_num);
-				start=check_if_room_available(start,room_num);
+				// printf("\nENTER ROOM NUMBER\n");
+				// scanf("%d",&room_num);
+				// start=check_if_room_available(start,room_num);
 				break;
 			case 0:
 				exit(1);
@@ -77,24 +77,58 @@ struct node *edit_info(struct node *start,int guestid)
 	temp = start;
 	while(temp!=NULL){
 		if(temp->guest_id == guestid){
-			printf("Enter the New name: ");
+			printf("\n Enter the New name: ");
 			scanf("%s",alias);
 			strcpy(temp->name,alias);
-			printf("Enter the new address");
+			printf("\n Enter the new address: ");
 			scanf("%[^\n]%*c", alias);
 			strcpy(temp->addr,alias);
-			printf("Enter the new age: ");
+			printf("\n Enter the new age: ");
 			scanf("%d",&a);
 			temp->age = a;
-			printf("Enter the new room number");
+			printf("\n Enter the new room number: ");
 			scanf("%d",&a);
 			temp->room_number = a;
 			return start;
 		}
 		temp=temp->link;
 	}
-	printf("guest id not found in our records");
+	printf("\n Guest id didn't match our records");
+	return start;	
+}
+
+struct node *checkout(struct node *start ){
+	int guest;
+	printf("\nEnter the guest id: ");
+	scanf("%d",&guest);
+	struct node *temp;
+	temp = start;
+	struct node * p;
+	p = (struct node *)malloc(sizeof(struct node));
+	if(start == NULL){
+		printf("\n You need to checkin first in order to checkout! ");
+		return start;
+	}
+
+	if(temp->guest_id==guest)
+	{
+		temp==NULL;
+		printf("\nHi %s !\n Thank you so much for choosing to stay at our place while in  %s. We hope you had a great time here and enjoyed your stay.", temp->name, temp->addr);
+		return start;
+	}
+	while (temp!=NULL)
+	{
+		if(temp->link->guest_id == guest){
+			p = temp->link;
+			temp->link = temp->link->link;
+			free(p);
+			printf("Hi %s !\n Thank you so much for choosing to stay at our place while in  %s. We hope you had a great time here and enjoyed your stay.", p->name, p->addr);
+			return start;
+		}
+		temp = temp->link;
+	}
+	printf("\nGuest id didn't match our records\n");
 	return start;
-}						
+}
 		
 	
